5cc8032665d261f8b3829e93b7b72c0900cf9e8a
Ngahoang2005 <hoangthanhnga2005@gmail.com>
2025-09-08 21:48:24 +0700

    Update lwf.py

--- models/lwf.py
+++ models/lwf.py
@@ -619,13 +619,7 @@ class LwF(BaseLearner):
             for cycle in range(12):  # 32 chu kỳ
                 # === 4 bước INNER ===
                 theta_t = {n: p.clone().detach() for n, p in self._network.named_parameters() if "fc" not in n}
-                for _ in range(1):
-                    try:
-                        _, inputs, targets = next(data_iter)
-                    except StopIteration:
-                        data_iter = iter(train_loader)
-                        _, inputs, targets = next(data_iter)
-
+                for _ in range(4):
                     inputs, targets = inputs.to(self._device), targets.to(self._device)
                     student_outputs = self._network(inputs)["logits"]
                     fake_targets = targets - self._known_classes
@@ -643,7 +637,7 @@ class LwF(BaseLearner):
                 theta_after_inner = {n: p.clone().detach() for n, p in self._network.named_parameters() if "fc" not in n}
                 delta_in = {n: theta_after_inner[n] - theta_t[n] for n in theta_t}
                 # === 1 bước OUTER ===
-                for _ in range(6): 
+                for _ in range(4): 
                     inputs, targets = inputs.to(self._device), targets.to(self._device)
                     logits = self._network(inputs)["logits"]
                     fake_targets = targets - self._known_classes
@@ -656,7 +650,7 @@ class LwF(BaseLearner):
                     T,
                     )
 
-                    loss = 5*loss_kd + loss_clf
+                    loss = loss_kd 
                     optimizer.zero_grad()
                     loss.backward()
                     self.ipt_score.update_outer_score(self._network, epoch)
@@ -670,7 +664,6 @@ class LwF(BaseLearner):
                 theta_after_outer = {n: p.clone().detach() for n, p in self._network.named_parameters() if "fc" not in n}
                 delta_out = {n: theta_after_outer[n] - theta_after_inner[n] for n in theta_t}
                 self.update_parameters_with_task_vectors(theta_t, delta_in, delta_out) 
-                theta_t = {n: p.clone().detach() for n, p in self._network.named_parameters() if "fc" not in n}
             # ---- epoch end ----
             scheduler.step()
             train_acc = np.around(tensor2numpy(torch.tensor(correct)) * 100 / total, decimals=2)
