5bd5a482461e3e8d1558d06a3e446a688bb5ad8e
Ngahoang2005 <hoangthanhnga2005@gmail.com>
2025-09-12 23:08:06 +0700

    Update lwf.py

--- models/lwf.py
+++ models/lwf.py
@@ -370,10 +370,10 @@ class LwF(BaseLearner):
 
             data_iter = iter(train_loader)
 
-            for cycle in range(12):  # 32 chu kỳ
+            for cycle in range(8):  # 32 chu kỳ
                 # === 4 bước INNER ===
                 theta_t = {n: p.clone().detach() for n, p in self._network.named_parameters() if "fc" not in n}
-                for _ in range(1):
+                for _ in range(2):
                     try:
                         _, inputs, targets = next(data_iter)
                     except StopIteration:
