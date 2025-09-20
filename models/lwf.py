fa57a31afa78f87075695efaa603be36b630cef4
Ngahoang2005 <hoangthanhnga2005@gmail.com>
2025-09-20 08:38:34 +0700

    Update lwf.py

--- models/lwf.py
+++ models/lwf.py
@@ -203,7 +203,7 @@ class IPTScore:
 
         outer_mask = {}
         for n, score in ipt_score_dic_outer.items():
-            threshold = torch.quantile(score, 0.1)
+            threshold = torch.quantile(score, 0.2)
             outer_mask[n] = (score > threshold).float()
  
         return outer_mask
@@ -227,7 +227,7 @@ class IPTScore:
             ipt_score_dic_outer[n] = ipt_score
 
         all_scores = torch.cat([score.flatten() for score in ipt_score_dic_outer.values()])
-        threshold = torch.quantile(all_scores, 0.1)
+        threshold = torch.quantile(all_scores, 0.2)
 
         outer_mask = {}
         for n, score in ipt_score_dic_outer.items():
