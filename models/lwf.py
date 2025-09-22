a201ee2ce3677a5bd3e3e5b0f3415eb2dbf6aad9
Ngahoang2005 <hoangthanhnga2005@gmail.com>
2025-09-23 00:34:57 +0700

    Update lwf.py

--- models/lwf.py
+++ models/lwf.py
@@ -336,8 +336,8 @@ class LwF(BaseLearner):
             assert inner.shape == outer.shape, f"Mismatched shape for {n}: {inner.shape} vs {outer.shape}"
 
             both_one = (inner == 1) & (outer == 1)
-            inner[both_one] = 0.3
-            outer[both_one] = 0.7
+            inner[both_one] = 0.2
+            outer[both_one] = 0.8
             both_zero = (inner == 0) & (outer == 0)
             inner[both_zero] = 0.5
             outer[both_zero] = 0.5
