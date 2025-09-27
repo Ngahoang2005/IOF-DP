16326aa749f202b9a87a085e74677b225c2ba139
Ngahoang2005 <hoangthanhnga2005@gmail.com>
2025-09-27 21:30:20 +0700

    Update lwf.py

--- models/lwf.py
+++ models/lwf.py
@@ -34,10 +34,10 @@ init_weight_decay = 0.0005
 # lamda = 10
 
 # Tiny-ImageNet200
-epochs = 2 
+epochs = 100
 lrate = 0.001
 milestones = [45, 90]
-lrate_decay = 20.1
+lrate_decay = 0.1
 batch_size = 128
 weight_decay = 2e-4
 num_workers = 8
@@ -370,8 +370,8 @@ class LwF(BaseLearner):
             correct, total = 0, 0
 
             data_iter = iter(train_loader)
-            for _, cycle in enumerate(prog_bar):
-            #for cycle in range(94):  # 32 chu kỳ
+            #for _, cycle in enumerate(prog_bar):
+            for cycle in range(94):  # 32 chu kỳ
                 # === 4 bước INNER ===
                 try:
                     _, inputs, targets = next(data_iter)
